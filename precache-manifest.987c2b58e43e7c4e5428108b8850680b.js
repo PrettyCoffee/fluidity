d17fbf9efc566899f796d30da4d115382e6ce5af
PrettyCoffee <jonathan-holz@outlook.com>
2021-03-18 18:28:16 +0100

    Updates

---
+++ precache-manifest.987c2b58e43e7c4e5428108b8850680b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1991b1ae4f13645669a3cfe1ef9d634e",
+    "url": "/startpage/index.html"
+  },
+  {
+    "revision": "14be60401e83010366fa",
+    "url": "/startpage/static/css/main.e7033b8e.chunk.css"
+  },
+  {
+    "revision": "8f9e05510d4b1da6b9c6",
+    "url": "/startpage/static/js/2.5aca5ca1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/startpage/static/js/2.5aca5ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14be60401e83010366fa",
+    "url": "/startpage/static/js/main.354bcee9.chunk.js"
+  },
+  {
+    "revision": "7dcaaa047bb16b5cca6f",
+    "url": "/startpage/static/js/runtime-main.ae06b328.js"
+  },
+  {
+    "revision": "6214fef263bf351d32d599371642be62",
+    "url": "/startpage/static/media/FiraCode-Light.6214fef2.woff"
+  },
+  {
+    "revision": "a9fbb495313e4cb2265a1284392e9da6",
+    "url": "/startpage/static/media/FuturaLTLight.a9fbb495.woff"
+  },
+  {
+    "revision": "03400a86da3daf17b376b34055f6732f",
+    "url": "/startpage/static/media/pic_1.03400a86.jpg"
+  },
+  {
+    "revision": "42b93d95319153fe54b47953aad4ed1b",
+    "url": "/startpage/static/media/pic_2.42b93d95.jpg"
+  },
+  {
+    "revision": "8796fde5bffabc5215bc0aad44d1a142",
+    "url": "/startpage/static/media/pic_3.8796fde5.jpg"
+  },
+  {
+    "revision": "66ccb182a56965ca730c5fecb7f7862a",
+    "url": "/startpage/static/media/pic_4.66ccb182.jpg"
+  },
+  {
+    "revision": "6e9543cd93106257d6783bd347446d27",
+    "url": "/startpage/static/media/pic_5.6e9543cd.jpg"
+  },
+  {
+    "revision": "65b20253b182ec977b99709892c72524",
+    "url": "/startpage/static/media/pic_6.65b20253.jpg"
+  },
+  {
+    "revision": "6516acf69228e1450d3b7c6f1ce7f475",
+    "url": "/startpage/static/media/pic_7.6516acf6.jpg"
+  },
+  {
+    "revision": "ba1342f42d643e4f554eaf120c6a2a38",
+    "url": "/startpage/static/media/pic_8.ba1342f4.png"
+  }
+]);
