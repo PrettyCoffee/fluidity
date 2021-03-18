f9cef6086be7d561da4753c756c770d915e9f4ba
PrettyCoffee <jonathan-holz@outlook.com>
2021-03-18 20:17:16 +0100

    feat(Searchbar): Add searchbar icons

--- src/Startpage/Searchbar/Searchbar.tsx
+++ src/Startpage/Searchbar/Searchbar.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import styled from "@emotion/styled"
-import * as settingsHandler from "../Settings/settingsHandler"
+import styled from "@emotion/styled";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as settingsHandler from "../Settings/settingsHandler";
 
-const StyledSearchbar = styled.input`
+import google from "../../data/pictures/google.svg";
+import duckduckgo from "../../data/pictures/duckduckgo.svg";
+import qwant from "../../data/pictures/qwant.svg";
+
+const StyledSearchbarContainer = styled.div`
     position: fixed;
-    left: 50px;
+    left: 100px;
     right: 50px;
     bottom: 40px;
-
-    width: calc(100% - 100px);
+    height:min-content;
+    `;
+const StyledSearchbar = styled.input`
+    width: 100%;
     font-size: 30pt;
-
+    
     background-color: rgba(0,0,0,0);
     color: var(--default-color);
-    opacity: 0.3;
     transition: .3s;
     border: none;
     border-bottom: 2px solid var(--default-color);
+    opacity: 0.3;
     
     ::placeholder {
         color: var(--default-color);
@@ -28,19 +36,33 @@ const StyledSearchbar = styled.input`
     }
 `;
 
+const SearchIcon = styled.img`
+    position: fixed;
+    left: 35px;
+    bottom: 40px;
+    height: 2.9rem;
+    transition: .3s;
+`;
+
 export const Searchbar = () => {
-    const engine = settingsHandler.getValue("search-engine") || "duckduckgo.com/";
+    const engine: string = settingsHandler.getValue("search-engine") || "duckduckgo.com/";
+    const searchSymbol = engine.startsWith("google") ? google
+        : engine.startsWith("qwant") ? qwant
+            : duckduckgo;
 
     function redirectToSearch(query: string) {
-        window.location.replace("https://" + engine + "?q=" + query);
+        window.location.href = "https://" + engine + "?q=" + query;
     }
 
     return (
-        <StyledSearchbar
-            placeholder="Always stay clean!"
-            type="input"
-            onKeyUp={e => e.which === 13 && redirectToSearch(e.currentTarget.value)}
-            autoFocus={true}
-        />
+        <StyledSearchbarContainer>
+            <StyledSearchbar
+                placeholder="Always stay clean!"
+                type="input"
+                onKeyUp={e => e.which === 13 && redirectToSearch(e.currentTarget.value)}
+                autoFocus={true}
+            />
+            <SearchIcon src={searchSymbol} />
+        </StyledSearchbarContainer>
     );
 }
