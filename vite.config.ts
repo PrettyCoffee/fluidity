e27b0f6d94e939a6077f23549264b1bc176c5f80
PrettyCoffee <jonathan-holz@outlook.com>
2023-08-04 23:14:22 +0200

    chore: Replace CRA with vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { checker } from "vite-plugin-checker"
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: "eslint ./src",
+          dev: {
+            logLevel: ["error"],
+          },
+        },
+      }),
+    ],
+  }
+})
