61bd95a5a24fcfdc2ee42f1f28801797bb403eb5
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-01-28 23:07:31 +0100

    chore(vite): Don't run eslint in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { checker } from "vite-plugin-checker"
 
 export default defineConfig(() => {
   return {
     base: "./",
-    build: {
-      outDir: "build",
-    },
-    plugins: [
-      react(),
-      checker({
-        typescript: true,
-        eslint: {
-          lintCommand: "eslint ./src",
-          dev: {
-            logLevel: ["error"],
-          },
-        },
-      }),
-    ],
+    plugins: [react()],
   }
 })
